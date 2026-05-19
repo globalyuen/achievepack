1e161948fb6481e3101eefadaca35cf72e3918ae
Ryan Yuan <globalyuen@gmail.com>
2026-05-19 14:26:26 +0800

    fix: resolve react-helmet-async import in CustomVsStandardPackagingPage

--- src/pages/topics/CustomVsStandardPackagingPage.tsx
+++ src/pages/topics/CustomVsStandardPackagingPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
-import { Link } from 'react-helmet-async' // Fallback helper or react-router-dom Link
 import { Link as RouterLink } from 'react-router-dom'
 import { 
   ArrowLeftRight, 
